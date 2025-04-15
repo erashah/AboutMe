114ee0eba6607d6501dd5bd16c8493de525e7d1d
Era Shah <erashah001@gmail.com>
2025-04-15 02:31:06 -0400

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Era Shah on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
